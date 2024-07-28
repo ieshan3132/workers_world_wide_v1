7a8901f306fcf8c1c86e807aae48479ea4f528e8
ieshan3132 <90199288+ieshan3132@users.noreply.github.com>
2024-07-28 15:13:48 +0530

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create-project/', views.createProject, name='create_project'),
     path('update-project/<str:pk>/', views.updateProject, name='update_project'),
     path('delete-project/<str:pk>/', views.deleteProject, name='delete_project'),
+
+    path('profile_image/<str:pk>/', views.serve_profile_image, name='serve_profile_image'),
 ]
